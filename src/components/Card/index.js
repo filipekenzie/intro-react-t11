f7a2f903756db97ce9410ada0c0f4aaaa0f2a519
Filipe Gutierry <filipegutierry@MacBook-Air-de-Filipe.local>
2022-05-23 11:05:14 -0300

    Fisrt commit

---
+++ src/components/Card/index.js
@@ -0,0 +1,11 @@
+function Card() {
+  return (
+    <>
+      <p>React é legal</p>
+      <p>React é legal</p>
+      <p>React é legal</p>
+    </>
+  );
+}
+
+export default Card;
---
+++ src/components/Profile/index.js
@@ -0,0 +1,9 @@
+function Profile(){
+    return(
+        <>
+        <p>Filipe</p>
+        </>
+    )
+}
+
+export default Profile;
